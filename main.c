129d99318ae1432b37f5b379abe76ccf1e602b5a
RANIM <135613007+RanimZaouga@users.noreply.github.com>
2023-08-22 10:01:54 +0000

    simple shell

--- main.c
+++ main.c
@@ -3,6 +3,6 @@
 /* Entry point of the program */
 int main()
 {
-    run_shell(); // Start the shell
-    return 0; // Return success status
+    run_shell(); /* Start the shell */
+    return 0; /* Return success status */
 }
