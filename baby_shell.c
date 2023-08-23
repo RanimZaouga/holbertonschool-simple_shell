8e72af8eab75ab9bb86dc167ab8d3730f4c1c870
RANIM <135613007+RanimZaouga@users.noreply.github.com>
2023-08-23 10:25:33 +0000

    simple shell

--- baby_shell.c
+++ baby_shell.c
@@ -1,6 +1,6 @@
 #include "shell.h"
-/*
- * main - A simple Unix command line interpreter (baby shell)
+/**
+ * main - Entry point for the simple Unix command line interpreter (baby shell).
  *
  * This program implements a basic Unix shell that reads commands from the user,
  * executes them if they are valid executables, and repeats the process until the
@@ -8,49 +8,63 @@
  * one-word commands without arguments. It displays a prompt, waits for user input,
  * and provides simple error messages for invalid inputs and non-executable commands.
  *
- * Features:
- * - Displays a prompt for user input
- * - Executes valid executable commands
- * - Handles "end of file" condition (Ctrl+D)
- * - Provides error messages for invalid commands
- * - Supports one-word commands without arguments
+ * @return Always returns 0 to indicate successful execution.
  */
- 
 int main(void)
 {
-    char command[1024];
+    char command[MAX_INPUT_LENGTH];
+    
     while (1)
-	{
-        printf("simple_shell> ");
-        fgets(command, 1024, stdin);
-        /* Check if user entered "end of file" (Ctrl+D) */
-        if (strcmp(command, "EOF\n") == 0)
-		{
-            printf("Exiting...\n");
-            break;
-        }
-        /* Check if command is empty */
-        if (strcmp(command, "\n") == 0)
-		{
+    {
+        printf("simple_shell> "); /* Display shell prompt */
+        if (fgets(command, sizeof(command), stdin) == NULL)
+        {
+            if (feof(stdin))
+            {
+                printf("\nExiting...\n"); /* Handle Ctrl+D (EOF) */
+                break;
+            }
+            perror("fgets"); /* Handle fgets error */
             continue;
         }
-        /* Check if command is a valid executable */
-        if (access(command, X_OK) != 0)
-		{
-            printf("Error: '%s' is not a valid executable.\n", command);
-            continue;
+
+        /* Remove trailing newline character */
+        command[strcspn(command, "\n")] = '\0';
+
+        if (strcmp(command, "") == 0)
+        {
+            continue; /* Empty command, continue to the next iteration */
         }
-        /* Execute command */
-        if (fork() == 0)
-		{
-            execve(command, NULL, NULL);
-            perror("Error");
-            exit(1);
+
+        pid_t child_pid = fork();
+        if (child_pid < 0)
+        {
+            perror("fork"); /* Handle fork error */
         }
-		else
-		{
-            wait(NULL);
+        else if (child_pid == 0)
+        {
+            /* Child process */
+            if (execlp(command, command, NULL) == -1)
+            {
+                perror("exec"); /* Handle exec error */
+                exit(EXIT_FAILURE);
+            }
+        }
+        else
+        {
+            /* Parent process */
+            int status;
+            waitpid(child_pid, &status, 0);
+            if (WIFEXITED(status))
+            {
+                printf("Child process exited with status %d\n", WEXITSTATUS(status));
+            }
+            else if (WIFSIGNALED(status))
+            {
+                printf("Child process terminated by signal %d\n", WTERMSIG(status));
+            }
         }
     }
+    
     return (0);
 }
