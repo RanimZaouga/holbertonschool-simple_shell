39e36e36d07036e4a573752eb1367f11c082dbbf
RANIM <135613007+RanimZaouga@users.noreply.github.com>
2023-08-23 10:57:10 +0000

    simple shell

--- baby_shell.c
+++ baby_shell.c
@@ -1,70 +1,72 @@
 #include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+/**
+ * run_shell - Run a simple Unix command line interpreter (baby shell).
+ *
+ * This function implements a basic Unix shell that reads commands from the user,
+ * executes them if they are valid executables, and repeats the process until the
+ * user enters the "exit" command. The shell supports simple one-word commands without arguments.
+ * It displays a prompt, waits for user input, and provides error messages for invalid inputs
+ * and non-executable commands.
+ */
+void run_shell(void)
+{
+    char command[100];
+    pid_t pid;
+
+    while (1)
+    {
+        printf("simple_shell> "); /* Display shell prompt */
+
+        /* Read user input */
+        scanf("%s", command);
+
+        /* Check if user entered "exit" command */
+        if (strcmp(command, "exit") == 0)
+        {
+            break; /* Exit the shell */
+        }
+
+        /* Create a child process */
+        pid = fork();
+
+        if (pid == 0)
+        {
+            /* Child process */
+            execlp(command, command, NULL); /* Execute the command */
+            perror("Error"); /* Handle exec error */
+            exit(1); /* Exit child process with error status */
+        }
+        else if (pid > 0)
+        {
+            /* Parent process */
+            wait(NULL); /* Wait for child process to complete */
+        }
+        else
+        {
+            /* Fork error */
+            perror("Error"); /* Handle fork error */
+            exit(1); /* Exit with error status */
+        }
+    }
+}
 /**
  * main - Entry point for the simple Unix command line interpreter (baby shell).
  *
  * This program implements a basic Unix shell that reads commands from the user,
  * executes them if they are valid executables, and repeats the process until the
- * user enters the "end of file" (Ctrl+D) signal. The shell supports simple,
- * one-word commands without arguments. It displays a prompt, waits for user input,
- * and provides simple error messages for invalid inputs and non-executable commands.
+ * user enters the "exit" command. The shell supports simple one-word commands without arguments.
+ * It displays a prompt, waits for user input, and provides error messages for invalid inputs
+ * and non-executable commands.
  *
- * @return Always returns 0 to indicate successful execution.
+ * Return: Always returns 0 to indicate successful execution.
  */
 int main(void)
 {
-    char command[MAX_INPUT_LENGTH];
-    
-    while (1)
-    {
-        printf("simple_shell> "); /* Display shell prompt */
-        if (fgets(command, sizeof(command), stdin) == NULL)
-        {
-            if (feof(stdin))
-            {
-                printf("\nExiting...\n"); /* Handle Ctrl+D (EOF) */
-                break;
-            }
-            perror("fgets"); /* Handle fgets error */
-            continue;
-        }
-
-        /* Remove trailing newline character */
-        command[strcspn(command, "\n")] = '\0';
-
-        if (strcmp(command, "") == 0)
-        {
-            continue; /* Empty command, continue to the next iteration */
-        }
-
-        pid_t child_pid = fork();
-        if (child_pid < 0)
-        {
-            perror("fork"); /* Handle fork error */
-        }
-        else if (child_pid == 0)
-        {
-            /* Child process */
-            if (execlp(command, command, NULL) == -1)
-            {
-                perror("exec"); /* Handle exec error */
-                exit(EXIT_FAILURE);
-            }
-        }
-        else
-        {
-            /* Parent process */
-            int status;
-            waitpid(child_pid, &status, 0);
-            if (WIFEXITED(status))
-            {
-                printf("Child process exited with status %d\n", WEXITSTATUS(status));
-            }
-            else if (WIFSIGNALED(status))
-            {
-                printf("Child process terminated by signal %d\n", WTERMSIG(status));
-            }
-        }
-    }
-    
+    run_shell();
     return (0);
 }
