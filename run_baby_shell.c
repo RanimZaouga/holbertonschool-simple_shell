b4238460fda9898e45fec2edd1f33db2592cb13e
RANIM <135613007+RanimZaouga@users.noreply.github.com>
2023-08-23 10:14:44 +0000

    simple shell

--- run_baby_shell.c
+++ run_baby_shell.c
@@ -21,7 +21,7 @@ void execute_command(const char *command)
     else if (child_pid == 0)
     {
         // Child process
-        char *args[] = {command, NULL}; // Create an argv array for execvp
+        char *args[] = {(char *)command, NULL}; // Cast away const for execvp
         execvp(command, args);
         
         perror("execvp");
@@ -43,6 +43,7 @@ void execute_command(const char *command)
     }
 }
 
+
 /*
  * run_shell - A simple Unix command line interpreter
  *
@@ -51,49 +52,33 @@ void execute_command(const char *command)
  * user enters the "end of file" (Ctrl+D) signal. The shell supports simple,
  * one-word commands without arguments.
  */
-
+void run_shell()
 /* Run the shell loop */
-void run_shell(void)
 {
-    char command[MAX_INPUT_LENGTH];
+    char input[MAX_INPUT_LENGTH];
+    
     while (1)
     {
-        printf("simple_shell> ");
-        if (fgets(command, MAX_INPUT_LENGTH, stdin) == NULL)
+        printf("simple_shell> "); /* Display shell prompt */
+        
+        // Read user input
+        if (fgets(input, sizeof(input), stdin) == NULL)
         {
-            printf("Exiting...\n");
-            break;
-        }
-
-        /* Remove newline character from the command */
-        command[strcspn(command, "\n")] = '\0';
-
-        /* Check if command is empty */
-        if (strcmp(command, "") == 0)
-        {
+            if (feof(stdin))
+            {
+                printf("\nExiting the shell.\n"); /* Handle Ctrl+D (EOF) */
+                break;
+            }
+            perror("fgets"); /* Handle fgets error */
             continue;
         }
-
-        /* Check if command is a valid executable */
-        if (access(command, X_OK) != 0)
-        {
-            printf("Error: '%s' is not a valid executable.\n", command);
-            continue;
-        }
-
-        /* Execute command */
-        if (fork() == 0)
-        {
-            execl(command, command, NULL); /* Execute with only the command itself as an argument */
-            perror("Error");
-            exit(1);
-        }
-        else
-        {
-            wait(NULL);
-        }
+        
+        input[strcspn(input, "\n")] = '\0'; /* Remove newline character */
+        
+        execute_command(input); /* Execute the entered command */
     }
 }
+
 
 int main(void)
 {
