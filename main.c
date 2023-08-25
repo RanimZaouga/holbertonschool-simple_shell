65fa7c73dc492e32df36f32231bb21631acda363
RANIM <135613007+RanimZaouga@users.noreply.github.com>
2023-08-25 01:26:44 +0000

    simle shell

--- main.c
+++ main.c
@@ -2,10 +2,13 @@
 /**
  * main - Entry point for the simple Unix command line interpreter (baby shell).
  *
- * This program implements a basic Unix shell that reads commands from the user,
- * executes them if they are valid executables, and repeats the process until the
- * user enters the "exit" command. The shell supports simple one-word commands without arguments.
- * It displays a prompt, waits for user input, and provides error messages for invalid inputs
+ * This program implements a basic Unix shell that
+ * reads commands from the user,executes them if they are valid executables,
+ * and repeats the process until the
+ * user enters the "exit" command.
+ * The shell supports simple one-word commands without arguments.
+ * It displays a prompt, waits for user input,
+ * and provides error messages for invalid inputs
  * and non-executable commands.
  *
  * Return: Always returns 0 to indicate successful execution.
@@ -13,6 +16,6 @@
 /* Entry point of the program */
 int main()
 {
-    run_shell(); /* Start the shell */
-    return (0); /* Return success status */
+	run_shell(); /* Start the shell */
+	return (0); /* Return success status */
 }
